c635b9ea2ea38f3304a5b1dd9bbb2f5ddb114750
nbd680 <nbd680@cs.bham.ac.uk>
2018-10-28 12:19:06 +0000

    Added the alternative resampling method given in the paper I read.  Read the comment in the code to see how it differs.

--- particleFilterStuff/particleFilterResampling.py
+++ particleFilterStuff/particleFilterResampling.py
@@ -2,6 +2,7 @@
 of samples that are supposed to be output.'''
 
 import random
+import math
 
 def resample(particles, n):
     totalWeight = 0
@@ -26,5 +27,32 @@ def resample(particles, n):
             sumSoFar = sumSoFar + particles[counter][1]
     return output
 
+'''Below is the alternative method of resampling given in the paper I read.
+It has the advantage of being much less random, so the results will be
+more consistent.  The downside is that you can't guarantee how big the output
+will be - probably about the same size as the input, but not exactly.'''
+
+def liuResample(particles):
+    rooted = []
+    n = len(particles)
+    total = 0
+    output = []
+    for i in range(n):
+        rooted.append(math.sqrt(particles[i][1]))
+        total = total + rooted[i]
+    factor = n/total
+    for i in range(n):
+        aj = (rooted[i] * factor)
+        if(aj > 1):
+            k = math.floor(aj)
+            for j in range(k):
+                output.append(particles[i])
+        else:
+            if(random.uniform(0.0, 1.0) < aj):
+                output.append(particles[i])
+    return output
+    
+
 '''Test'''
-print(resample([('a', 12), ('b', 2), ('c', 5)], 50))
+print(resample([('a', 12), ('b', 2), ('c', 5)], 5))
+print(liuResample([('a', 12), ('b', 2), ('c', 5)]))
