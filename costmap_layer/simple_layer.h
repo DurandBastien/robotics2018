007e5081d7d6f71e64717e481f7d15b1316a63b3
Bastien <bastien.durand@insa-lyon.fr>
2018-12-03 11:15:38 +0000

    costmap layer to see boxes as obstacles

---
+++ costmap_layer/simple_layer.h
@@ -0,0 +1,33 @@
+#ifndef SIMPLE_LAYER_H_
+#define SIMPLE_LAYER_H_
+#include <ros/ros.h>
+#include "geometry_msgs/PoseStamped.h"
+#include <costmap_2d/layer.h>
+#include <costmap_2d/layered_costmap.h>
+#include <costmap_2d/GenericPluginConfig.h>
+#include <dynamic_reconfigure/server.h>
+
+namespace simple_layer_namespace
+{
+
+class SimpleLayer : public costmap_2d::Layer
+{
+public:
+  SimpleLayer();
+
+  virtual void onInitialize();
+  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
+                             double* max_y);
+  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+
+private:
+  ros::Subscriber or_sub;
+  void or_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+
+  double mark_x_, mark_y_;
+  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+};
+}
+#endif
